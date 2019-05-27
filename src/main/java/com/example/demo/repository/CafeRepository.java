02fc08b02bcdfaad230dc256e98ab1f7049dd840
supfeer <supfeer@gmail.com>
2019-05-27 15:00:32 +0300

    Init

--- src/main/java/com/example/demo/repository/CafeRepository.java
+++ src/main/java/com/example/demo/repository/CafeRepository.java
@@ -11,6 +11,5 @@ import java.util.List;
 @Repository
 public interface CafeRepository extends JpaRepository<Cafe, Integer> {
 
-    @Query("select c.name, (select avg (v.traffic) from Visit v where v.cafeId = c.id) as trafficAvgDay from Cafe c")
     List<TrafficAverage> getTrafficAvg();
 }
--- src/main/java/com/example/demo/service/CafeService.java
+++ src/main/java/com/example/demo/service/CafeService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import com.example.demo.model.TrafficAverage;
 import com.example.demo.repository.CafeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +17,7 @@ public class CafeService {
         this.cafeRepository = cafeRepository;
     }
 
+    @Query("select c.name,  avg (v.traffic) as trafficAvgDay from Cafe c left join Visit v on v.cafeId = c.id group by c.id")
     public List<TrafficAverage> getTrafficAvg(){
         return cafeRepository.getTrafficAvg();
     }
